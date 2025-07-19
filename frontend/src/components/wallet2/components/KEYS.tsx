31341e7671effdc3cf08402b3db9d30323419974
joejoetommy <joejoetommy@proton.me>
2025-07-19 01:52:00 +0100

    Having some issue with a reduction in the mb capacity on V552 , REINTEGRATING IN THE ORGINAL V45 to keep the MAX Mb push of MB

---
+++ frontend/src/components/wallet2/components/KEYS.tsx
@@ -0,0 +1,21 @@
+// 2nd key d3842ed475242f25f5839cf92043c8341d0e654e8d29119843a15fdaed9b2c43   mx3cGq6YBiH8rYJycgLeW1QGuPUnDoUrCj  TXID= 8eb02eb50cbf8a65df7722b33ce48f2fe1f50705e99e5bfe05c096b1e464e49f  8eb4cafed6b5a3e0a893441e3cc17146af8d32c6ee96498baa986dbaa763adaa     
+// 3rd key dff16672174569c206d9303c8ad427c4383addbcaa31991ea2a9f2cd2861def1   n1VtTgFqT6Kra6iY93YH8SA8ZqxvNC21Bj  TXID= 
+// 4th key   671d6de03a9dbee6c691b4cb5afdf219f3bfb3b70afef3e2ea8ee76f0196bd53  mw62mij7PZzdxhMHPx85EEp3XbwbuLGp9H TXID= 
+// 5th key   ef519bf189c89954dfe580e4bb22980d075157ed97a2751c5542ece55d76a44b    mo7CXYyBpNYVsRHY1LZhQiZ9nQr9Nws7dH TXID= 
+// 6th key 7e20f00bf555d908f64dc1326b7f20c5c4f1201f14d79b8a310e056f97599f67   mtu8w1L1eD35n625DpY4pngvD6XdM1kMZT TXID= 
+// 7th key 08cf0bba0b27030b1dd02bd01cd9293385878c08edd1eb53fe586d06f47476d8  n3qDYwh6hKKCiu5R7fX7GuWWgdyJ1RMc9r TXID= 
+// 8th key 853f50b1cd687d5d79b07ced6eeb0a3c7f99a62eedffa27eaed7cb5b0285310e   mnSPX9gQS2K8KCNPp9Vp3jzS3aBWgWRawc TXID= 
+// 9th key 3e426560b9bf599f4fc957bb15a9299bb12ef1b4afa17a53305edaa8a9ab811d   mvFSzgiWXqdQbxRzp4uHzWcmKQVVdV7o8G
+//////////////////////////////////////////
+
+// 1st key
+// 1de096339a813a63f3bb829ddbcd307086e426c498811986700c275f35c1ac04
+// mzoFnR9fpyLh3Sabvx3oiXDFtPspXKusms
+// 1st 1SAT
+// 6455ef4c2f45230cf29a8c09b920ca81e89a0b92ff54860b64d3c057760c4ac4 
+// {
+//   "type": "jesus is the only way",
+//   "message": "remember to go to him, all who are heavy laden and you will find rest"
+// }
+
+///////////////////////////////////
